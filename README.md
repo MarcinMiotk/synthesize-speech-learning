a57bec1894989a617c15b9beb55e62f822f415ed
Marcin Miotk <mami@snmill.com>
2017-12-29 08:04:37 +0100

    Synthesize example

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # synthesize-speech-learning
 Synthesize Speech
+
+# On-Line TTS
+
+https://us-west-2.console.aws.amazon.com/polly/home/SynthesizeSpeech
+
---
+++ src/main/java/speech/SynthesizeSpeechSample.java
@@ -0,0 +1,44 @@
+package speech;
+
+import com.amazonaws.services.polly.AmazonPolly;
+import com.amazonaws.services.polly.AmazonPollyClientBuilder;
+import com.amazonaws.services.polly.model.OutputFormat;
+import com.amazonaws.services.polly.model.SynthesizeSpeechRequest;
+import com.amazonaws.services.polly.model.SynthesizeSpeechResult;
+import com.amazonaws.services.polly.model.VoiceId;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class SynthesizeSpeechSample {
+    AmazonPolly client = AmazonPollyClientBuilder.defaultClient();
+
+    public void synthesizeSpeech() {
+        String outputFileName = "speech.mp3";
+
+        SynthesizeSpeechRequest synthesizeSpeechRequest = new SynthesizeSpeechRequest()
+                .withOutputFormat(OutputFormat.Mp3)
+                .withVoiceId(VoiceId.Brian)
+                .withText("This is a sample text to be synthesized.");
+
+        try (FileOutputStream outputStream = new FileOutputStream(new File(outputFileName))) {
+            SynthesizeSpeechResult synthesizeSpeechResult = client.synthesizeSpeech(synthesizeSpeechRequest);
+            byte[] buffer = new byte[2 * 1024];
+            int readBytes;
+
+            try (InputStream in = synthesizeSpeechResult.getAudioStream()){
+                while ((readBytes = in.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, readBytes);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Exception caught: " + e);
+        }
+    }
+
+    public static void main(String... args) {
+        SynthesizeSpeechSample app = new SynthesizeSpeechSample();
+        app.synthesizeSpeech();
+    }
+}
